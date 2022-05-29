179b2bf816fadcc810d254d3546cf2d5721e0b09
Anees Asghar <aneesasgharsyed@gmail.com>
2022-05-29 14:19:48 +0100

    Code cleanup / commenting

--- main.c
+++ main.c
@@ -4,92 +4,100 @@
 #include <math.h>
 #include <pthread.h>
 
+
+// Returns 1 if point (x, y) is on or inside the circle of radius 1, otherwise returns 0
 int point_in_circle(float x, float y) {
-    float d = sqrt(x*x + y*y);
-    return (d <= 1.0) ? 1 : 0;
+    float distance_from_origin = sqrt(x*x + y*y);
+    return (distance_from_origin <= 1.0) ? 1 : 0;
 }
 
 
+// Returns a random float between the range min_value and max_value
 float rand_float(float min_value, float max_value) {
-    float norm_rand_float = rand() / (float) RAND_MAX; // 0.0 and 1.0
-    float rand_float = norm_rand_float * (max_value - min_value) + min_value; // -1.0 and 1.0
+    float norm_rand_float = rand() / (float) RAND_MAX; // random float between 0.0 and 1.0
+    float rand_float = norm_rand_float * (max_value - min_value) + min_value; // random float in the requested range
     return rand_float;
 }
 
 
+// Thread routine to generate a specified number of points and return a pointer to the 
+// total number of those points that fall on or inside the circle of radius 1
 void *routine(void *param) {
+    int *p_n_points = (int *)param; // pointer to the number of points to generate
     int i = 0, n_points_in_circle = 0;
-    int *n_points_p = (int *) param;
-    int* presult = malloc(sizeof(int*));
+    int* p_result = malloc(sizeof(int*)); // allocate space in memory for the result (n_points_in_circle)
 
-    for (i = 0; i < *n_points_p; i++) {
-        float x = rand_float(-1, 1), y = rand_float(-1, 1);
-        int is_point_in_circle = point_in_circle(x, y);
-
-        if (is_point_in_circle) {
+    // generate points and increment n_points_in_circle if point falls on or inside the circle
+    for (i = 0; i < *p_n_points; i++) {
+        float x = rand_float(-1.0, 1.0), y = rand_float(-1.0, 1.0);
+        if (point_in_circle(x, y)) {
             n_points_in_circle++;
         }
     }
 
-    *presult = n_points_in_circle;
-    return (void*) presult;
+    *p_result = n_points_in_circle;
+    return (void*)p_result;
 }
 
+
+// Simulates monte carlo with n_points points across n_threads threads
+// and returns an approximation of pi
 float monte_carlo(int n_points, int n_threads) {
-    int i = 0, n_total_points_in_circle = 0;
     double pi;
-    
-    pthread_t *threads = malloc(n_threads * sizeof(pthread_t));
-    int **result = malloc(n_threads * sizeof(int *));
+    int i, total_n_points_in_circle = 0;
+    int n_points_per_thread  = ceil((double)n_points / n_threads); // calculate the number of random points each thread has to generate
+    n_points = n_points_per_thread * n_threads;
 
-    int n_points_per_thread  = ceil(n_points / n_threads);
+    pthread_t *thread_ids = malloc(n_threads * sizeof(pthread_t)); // array of size n_threads to store pthread_t
+    int **result = malloc(n_threads * sizeof(int *)); // array of size n_threads where each element stores the
+                                                      // return value of a thread
 
+    // create threads
     for (i = 0; i < n_threads; i++) {
-        pthread_create(threads+i, NULL, &routine, (void *) &n_points_per_thread);
+        pthread_create(thread_ids+i, NULL, &routine, (void *)&n_points_per_thread);
     }
 
+    // await threads
     for (i = 0; i < n_threads; i++) {
-        pthread_join(threads[i], (void **) result+i);
+        pthread_join(thread_ids[i], (void **) result+i);
     }
 
+    // calculate total number of points in circle (across all threads)
     for (i = 0; i < n_threads; i++) {
-        n_total_points_in_circle += *result[i];
+        total_n_points_in_circle += *result[i];
     }
 
-    free(threads);
+    // free dynamically allocated memory
+    free(thread_ids);
     for (i = 0; i < n_threads; i++) {
-        free(result[i]);
+        free(result[i]); // allocated inside thread routine
     }
     free(result);
 
-    pi = (n_total_points_in_circle / (double)(n_points_per_thread * n_threads)) * 4;
+    pi = ((double)total_n_points_in_circle / n_points) * 4; // compute pi
+    
     return pi;
 }
+
 
 int main() {
     srand(time(NULL));
 
-    int i, j;
-    float error_rate;
+    int i, j, n_points, n_threads;
+    double approx_pi;
+    int thread_options[] = {2, 4, 6, 8};
+    int point_options[] = {20000, 100000, 1000000, 10000000};
 
-    int n_threads_settings[] = {2, 4, 6, 8};
-    int n_points_settings[] = {20000, 100000, 1000000, 10000000};
-
+    // simulate monte carlo and print performance metrics for each pair of options
     for (i = 0; i < 4; i++){
-        int n_points = n_points_settings[i];
         for (j = 0; j < 4; j++) {
-            int n_threads = n_threads_settings[j];
+            n_points = point_options[i];
+            n_threads = thread_options[j];
 
-            double approx_pi = monte_carlo(n_points, n_threads);
-
-            // error_rate = (M_PI - approx_pi) / M_PI * 100;
-
-            // printf("--------------------------------------\n");
-            // printf("Settings:\tn_threads = %d\tn_points = %d\n", n_threads, n_points);
-            // printf("The approximation of pi using these settings is %lf.\n", approx_pi);
-            // printf("The error rate for this approximation of pi is %f%%\n", error_rate);
-
-            printf("For %d threads and %d points the approximation of pi is %lf\n", n_threads, n_points, approx_pi);
+            approx_pi = monte_carlo(n_points, n_threads);
+            printf("No. of points: %d\n", n_points);
+            printf("No. of threads: %d\n", n_threads);
+            printf("Approximation of pi: %lf\n\n", approx_pi);
         }
     }
 
